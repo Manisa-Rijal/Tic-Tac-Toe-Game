0fdb4323371c88a67b600f2f14025bc7c39528f4
Manisa-Rijal <manisa7rijal@gmail.com>
2025-07-27 22:22:19 +0530

    Add files via upload

---
+++ Tic Tac Toe Game/src/Main.java
@@ -0,0 +1,77 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = ' ';
+            }
+        }
+
+        char player = 'X';
+        boolean gameOver = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while (!gameOver) {
+            printBoard(board);
+            System.out.println("Player " + player + ", enter your move (row and column): ");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+                board[row][col] = player;
+                gameOver = checkWin(board, player);
+
+                if (gameOver) {
+                    printBoard(board);
+                    System.out.println("Player " + player + " wins!");
+                } else {
+                    player = (player == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move, try again!");
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static void printBoard(char[][] board) {
+        System.out.println("-------------");
+        for (int row = 0; row < board.length; row++) {
+            System.out.print("| ");
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print(board[row][col] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public static boolean checkWin(char[][] board, char player) {
+        // Check rows
+        for (int row = 0; row < 3; row++) {
+            if (board[row][0] == player && board[row][1] == player && board[row][2] == player)
+                return true;
+        }
+
+        // Check columns
+        for (int col = 0; col < 3; col++) {
+            if (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+                return true;
+        }
+
+        // Check diagonals
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player)
+            return true;
+
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player)
+            return true;
+
+        return false;
+    }
+}
